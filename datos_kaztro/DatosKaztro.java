62aca888b61ec62beb31b9df26438d80897edca8
Kaztro <37382584+kaztro@users.noreply.github.com>
2018-03-14 16:49:24 -0600

    Add files via upload

---
+++ datos_kaztro/DatosKaztro.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author kaztro
+ */
+public class DatosKaztro {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: German Alexander");
+        System.out.println("Apellidos: Castro Portillo");
+        System.out.println("Fecha de nacimiento: 12/03/1999");
+        System.out.println("Edad: 18");
+        System.out.println("Ocupacion: Estudiante");
+        System.out.println("Estado civil: Soltero");
+        System.out.println("Universidad en la que estudia: UCA");
+        System.out.println("N. Carne: 00229017");
+        System.out.println("Correo institucional: 00229017@@uca.edu.sv");
+    }
+    
+}
